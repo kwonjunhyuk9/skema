0001e55e8d288f18481d43b188051596524a0664
kwonjunhyuk9 <kwonjunhyuk9@icloud.com>
2025-04-03 21:28:01 +0900

    Added aside bar and dynamic routing

---
+++ script.js
@@ -0,0 +1,8 @@
+function loadPage(page) {
+    fetch(`${page}/${page}.html`)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("content").innerHTML = html;
+        })
+        .catch(error => console.error("page load error:", error));
+}
