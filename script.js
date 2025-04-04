6875da733361ff5d0013bd6d6b8dc49428cef61b
kwonjunhyuk9 <kwonjunhyuk9@icloud.com>
2025-04-04 23:22:37 +0900

    Added blog data for html

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// 기본 페이지 로딩하기
+window.addEventListener('DOMContentLoaded', () => {
+    loadPage('profile');
+});
+
+// 함수 호출 시 페이지 전환하기
 function loadPage(page) {
     fetch(`${page}/${page}.html`)
         .then(response => response.text())
