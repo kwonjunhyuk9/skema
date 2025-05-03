813b1b53ea07c7d276787cbf0ab03d57d7ac5df8
kwonjunhyuk9 <kwonjunhyuk9@icloud.com>
2025-05-03 23:17:25 +0900

    Added Functional Programming, Object Oriented Programming, Built In Objects, Tools Folder

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// 기본 페이지 로딩하기
 window.addEventListener('DOMContentLoaded', () => {
     loadPage('profile');
 });
 
-// 함수 호출 시 페이지 전환하기
 function loadPage(page) {
     fetch(`${page}/${page}.html`)
         .then(response => response.text())
